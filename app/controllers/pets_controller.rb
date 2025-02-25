03c5988fd09b4c61234413be3da7c8dc308951fe
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-02-25 17:56:56 +0200

    create with simple form

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,36 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def pet_params
+    params.require(:pet).permit(:name, :species, :address, :date)
+  end
+end
